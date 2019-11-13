d8eeb1571de680f215ac7c9f2ce6e93c7e9286f4
danwan754 <danwan754@gmail.com>
2019-11-13 02:35:12 -0800

    Add Review component

---
+++ src/components/Review.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+
+import SortReview from './SortReview';
+import '../css/Review.css';
+import Star from '../assets/star.png';
+
+class Review extends React.Component {
+
+    constructor() {
+        super();
+        this.renderStars = this.renderStars.bind(this);
+    }
+
+    renderStars(length) {
+        let stars = [];
+        for (var i = 0; i < length; i++) {
+            stars.push(
+                <img key={i}
+                    className='star'
+                    src={Star} 
+                    alt={'no image'}>
+                </img>
+            );
+        }
+        return stars;
+    }
+
+    render() {
+        let reviews = this.props.reviews;
+        return (
+            <div className="reviews-container">
+                <SortReview />
+                { reviews.map( (review, index) => (
+                    <div className="review" key={index} >
+                        <p>{ review.author_name }</p>
+                        <div className="rating-time-container">
+                            <span className="star-container">
+                                { this.renderStars(review.rating) }
+                            </span>
+                            <span className="time-description">
+                                { review.relative_time_description }
+                            </span>
+                        </div>
+                        <p>{ review.text }</p>
+                    </div>
+                ))}
+            </div>
+        )
+    }
+}
+
+export default Review;
